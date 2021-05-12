2abba06c67fa347280fe34f784556277b411d62a
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2021-05-12 09:07:03 +0530

    Added the bottom navigation tab pages.

---
+++ lib/TabPages/EarningsTabPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EarningsTabPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Earnings Tab"),
+    );
+  }
+}
