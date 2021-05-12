ae476a1b0b68a63dc4da06a01a27b2a65d68e7f3
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2021-05-12 09:07:52 +0530

    Fixed some bugs.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:driver_app/DataHandler/AppData.dart';
+import 'package:driver_app/Screens/CarInfoScreen.dart';
 import 'package:driver_app/Screens/LoginScreen.dart';
 import 'package:driver_app/Screens/MainScreen.dart';
 import 'package:driver_app/Screens/RegisterScreen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +17,9 @@ void main() async {
 DatabaseReference usersRef =
     FirebaseDatabase.instance.reference().child("users");
 
+DatabaseReference driversRef =
+    FirebaseDatabase.instance.reference().child("drivers");
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -29,13 +32,12 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: RegistrationScreen(),
-        initialRoute: FirebaseAuth.instance.currentUser == null
-            ? LoginScreen.screenId
-            : MainScreen.screenId,
+        initialRoute: MainScreen.screenId,
         routes: {
           RegistrationScreen.screenId: (context) => RegistrationScreen(),
           LoginScreen.screenId: (context) => LoginScreen(),
           MainScreen.screenId: (context) => MainScreen(),
+          CarInfoScreen.screenId: (context) => CarInfoScreen(),
           // SearchScreen.screenId: (context) => SearchScreen(),
         },
       ),
