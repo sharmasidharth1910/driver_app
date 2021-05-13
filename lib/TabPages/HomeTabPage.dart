fc6b4c2ff648ca9cf2119941fac55d4e7001e824
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2021-05-13 22:33:32 +0530

    Updated Home Page Tab.

--- lib/TabPages/HomeTabPage.dart
+++ lib/TabPages/HomeTabPage.dart
@@ -1,10 +1,110 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeTabPage extends StatelessWidget {
+  Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController newGoogleMapController;
+
+  Position currentPosition;
+  var geoLocator = Geolocator();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  Future<void> locatePosition() async {
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+    currentPosition = position;
+
+    LatLng latLng = LatLng(position.latitude, position.longitude);
+
+    CameraPosition cameraPosition = CameraPosition(
+      target: latLng,
+      zoom: 14,
+    );
+    newGoogleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    // String address = await AssistantMethods.searchCoordinateAddress(
+    //   position: position,
+    //   context: context,
+    // );
+    // print("Result from getAddress : " + address);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home Tab"),
+    return Stack(
+      children: [
+        GoogleMap(
+          mapType: MapType.normal,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: true,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controllerGoogleMap.complete(controller);
+            newGoogleMapController = controller;
+
+            locatePosition();
+          },
+        ),
+        Container(
+          height: 140.0,
+          width: double.infinity,
+          color: Colors.black54,
+        ),
+        Positioned(
+          top: 60.0,
+          left: 0.0,
+          right: 0.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                  ),
+                  onPressed: () {},
+                  child: Container(
+                    color: Colors.green,
+                    child: Padding(
+                      padding: EdgeInsets.all(17.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Online Now ",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Icon(
+                            Icons.phone_android,
+                            color: Colors.white,
+                            size: 26.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
