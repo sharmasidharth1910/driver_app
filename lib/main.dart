10df1c776252e532db841b8d6839b7cf9eb06af0
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2021-05-16 20:02:16 +0530

    Bug fixes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:driver_app/Credentials/ConfigMaps.dart';
 import 'package:driver_app/DataHandler/AppData.dart';
 import 'package:driver_app/Screens/CarInfoScreen.dart';
 import 'package:driver_app/Screens/LoginScreen.dart';
 import 'package:driver_app/Screens/MainScreen.dart';
 import 'package:driver_app/Screens/RegisterScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +13,9 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  currentfirebaseUser = FirebaseAuth.instance.currentUser;
+
   runApp(MyApp());
 }
 
@@ -19,6 +24,12 @@ DatabaseReference usersRef =
 
 DatabaseReference driversRef =
     FirebaseDatabase.instance.reference().child("drivers");
+
+DatabaseReference rideRequestRef = FirebaseDatabase.instance
+    .reference()
+    .child("drivers")
+    .child(currentfirebaseUser.uid)
+    .child("newRide");
 
 class MyApp extends StatelessWidget {
   @override
@@ -32,7 +43,9 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: RegistrationScreen(),
-        initialRoute: MainScreen.screenId,
+        initialRoute: FirebaseAuth.instance.currentUser == null
+            ? LoginScreen.screenId
+            : MainScreen.screenId,
         routes: {
           RegistrationScreen.screenId: (context) => RegistrationScreen(),
           LoginScreen.screenId: (context) => LoginScreen(),
